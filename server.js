9eb7993700da9fb0fc666c6093f2d1929a7c0c38
Morgan <morgan.willock@gmail.com>
2021-06-16 10:50:07 +0930

    Added /test route to check yaml parsing to js object. Added jjsonwebtoken dependancy and /api/login route to check authentication

--- server.js
+++ server.js
@@ -1,10 +1,40 @@
 const express = require("express");
 const app = express();
+const yaml = require('js-yaml');
+const fs = require('fs');
+const jwt = require('jsonwebtoken');
+const { json } = require("express");
 
 let port = 8080;
 
 app.get('/hello', (req, res) => {
     res.send('hello world: nodemon restart working')
+})
+
+app.get('/test', (req, res) => {
+    try {
+        let fileContents = fs.readFileSync('./exercise.yaml', 'utf8');
+        let data = yaml.load(fileContents);
+
+        console.log(data);
+    } catch (e) {
+        console.log(e);
+    }
+
+    res.send('test route for reading .yaml file. data console logged to server')
+})
+
+app.post('/api/login', (req, res) => {
+    const auth = {
+        username: 'john',
+        password: 'password123'
+    }
+
+    jwt.sign({ auth: auth }, 'secretkey', (err, token) => {
+        res.json({
+            token,
+        })
+    })
 })
 
 app.listen(port, () => {
