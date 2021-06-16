1e17c28b8d5e99cf5a44fd9a7d80fed973a788ef
Morgan <morgan.willock@gmail.com>
2021-06-16 11:49:48 +0930

    Added json parser dependancy. Implemented authUser function to check login credentials. Working with test case but requires further testing.

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const app = express();
 const yaml = require('js-yaml');
 const fs = require('fs');
 const jwt = require('jsonwebtoken');
-const { json } = require("express");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 let port = 8080;
 
@@ -37,6 +38,14 @@ app.post('/api/data', authToken, (req, res) => {
     })
 })
 
+app.post('/api/login', authUser, (req, res) => {
+    let user = req.body
+
+    jwt.sign({ user }, 'secretkey', (err, token) => {
+        res.json({ token })
+    })
+})
+
 function authToken(req, res, next) {
     const bearerHeader = req.headers['authorization']
     if (typeof bearerHeader !== 'undefined') {
@@ -48,16 +57,21 @@ function authToken(req, res, next) {
     } 
 }  
 
-app.post('/api/login', (req, res) => {
-    const auth = {
-        username: 'john',
-        password: 'password123'
-    }
-
-    jwt.sign({ auth: auth }, 'secretkey', (err, token) => {
-        res.json({ token })
+function authUser(req, res, next) {
+    let fileContents = fs.readFileSync('./exercise.yaml', 'utf8');
+    let data = yaml.load(fileContents);
+    
+    let user = req.body
+    let result = data.auth.filter(loginDetails => {
+        return loginDetails.password === user.password && loginDetails.username === user.username
     })
-})
+
+    if(result.length > 0) {
+        next()
+    } else {
+        res.sendStatus(403)
+    }
+}  
 
 app.listen(port, () => {
     console.log(`Now listening on port: ${port}`)
