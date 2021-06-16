7542fa25f6d30965fa4619935a2d658075185ca0
Morgan <morgan.willock@gmail.com>
2021-06-16 12:34:26 +0930

    Ran prettier over code for consistency

--- server.js
+++ server.js
@@ -1,69 +1,72 @@
 const express = require("express");
 const app = express();
-require('dotenv').config();
-const yaml = require('js-yaml');
-const fs = require('fs');
-const jwt = require('jsonwebtoken');
+require("dotenv").config();
+const yaml = require("js-yaml");
+const fs = require("fs");
+const jwt = require("jsonwebtoken");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 let port = 8080;
 
-app.get('/api', (req, res) => {
-    res.send('Welcome. api is functioning correctly')
-})
+app.get("/api", (req, res) => {
+  res.send("Welcome. api is functioning correctly");
+});
 
-app.post('/api/data', authToken, (req, res) => {
-    jwt.verify(req.token, process.env.JWT_SECRET, (err, authData) => {
-        if(err) {
-            res.sendStatus(403)
-        } else {
-            let fileContents = fs.readFileSync('./exercise.yaml', 'utf8');
-            let data = yaml.load(fileContents);
-            let customersData = data.customers
+app.post("/api/data", authToken, (req, res) => {
+  jwt.verify(req.token, process.env.JWT_SECRET, (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      let fileContents = fs.readFileSync("./exercise.yaml", "utf8");
+      let data = yaml.load(fileContents);
+      let customersData = data.customers;
 
-            res.json({
-                customersData
-            })
-        }
-    })
-})
+      res.json({
+        customersData,
+      });
+    }
+  });
+});
 
-app.post('/api/login', authUser, (req, res) => {
-    let user = req.body
+app.post("/api/login", authUser, (req, res) => {
+  let user = req.body;
 
-    jwt.sign({ user }, process.env.JWT_SECRET, (err, token) => {
-        res.json({ token })
-    })
-})
+  jwt.sign({ user }, process.env.JWT_SECRET, (err, token) => {
+    res.json({ token });
+  });
+});
 
 function authToken(req, res, next) {
-    const bearerHeader = req.headers['authorization']
-    if (typeof bearerHeader !== 'undefined') {
-        const bearerToken = bearerHeader.split(' ')[1]
-        req.token = bearerToken
-        next()
-    } else {
-        res.sendStatus(403)
-    }
+  const bearerHeader = req.headers["authorization"];
+  if (typeof bearerHeader !== "undefined") {
+    const bearerToken = bearerHeader.split(" ")[1];
+    req.token = bearerToken;
+    next();
+  } else {
+    res.sendStatus(403);
+  }
 }
 
 function authUser(req, res, next) {
-    let fileContents = fs.readFileSync('./exercise.yaml', 'utf8');
-    let data = yaml.load(fileContents);
-    
-    let user = req.body
-    let result = data.auth.filter(loginDetails => {
-        return loginDetails.password === user.password && loginDetails.username === user.username
-    })
+  let fileContents = fs.readFileSync("./exercise.yaml", "utf8");
+  let data = yaml.load(fileContents);
 
-    if(result.length > 0) {
-        next()
-    } else {
-        res.sendStatus(403)
-    }
+  let user = req.body;
+  let confirmUsernamePasswordMatchData = data.auth.filter((loginDetails) => {
+    return (
+      loginDetails.password === user.password &&
+      loginDetails.username === user.username
+    );
+  });
+
+  if (confirmUsernamePasswordMatchData.length > 0) {
+    next();
+  } else {
+    res.sendStatus(403);
+  }
 }
 
 app.listen(port, () => {
-    console.log(`Now listening on port: ${port}`)
-})
+  console.log(`Now listening on port: ${port}`);
+});
